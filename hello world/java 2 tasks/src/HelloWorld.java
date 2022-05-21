7b281aeddcd1a4da30ce467f8ad3396143d6d584
Sofonias Aberra <81432076+coder885@users.noreply.github.com>
2022-05-21 02:06:01 +0200

    Add files via upload

---
+++ hello world/java 2 tasks/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String args[]){
+
+        String Fname = "sofonias";
+        String Lname =  "Abrra";
+        int Album = 52877;
+
+        System.out.println("Hello world. my name is "+ Fname + " "+ Lname+ " my album number is "+ Album);
+    }
+}
