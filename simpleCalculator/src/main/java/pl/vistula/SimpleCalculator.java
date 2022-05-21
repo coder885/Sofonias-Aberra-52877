c8bef95d5cc6ff6dfc6e5c044378d7947cc175aa
Sofonias Aberra <81432076+coder885@users.noreply.github.com>
2022-05-21 02:08:06 +0200

    Add files via upload

---
+++ simpleCalculator/src/main/java/pl/vistula/SimpleCalculator.java
@@ -0,0 +1,27 @@
+package pl.vistula;
+
+public class SimpleCalculator implements Calculator {
+    @Override
+    public double add(double first, double second) {
+
+        return first + second;
+    }
+
+    @Override
+    public double sub(double first, double second) {
+
+        return first - second;
+    }
+
+    @Override
+    public double mul(double first, double second)
+    {
+        return first * second;
+    }
+
+    @Override
+    public double div(double first, double second)
+    {
+        return first / second ;
+    }
+}
